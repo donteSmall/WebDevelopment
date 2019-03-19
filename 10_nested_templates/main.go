137a6c455fb28cd67a8789ffe24678e685c8489b
Donte Small <small.donte@yahoo.com>
2019-03-19 07:24:19 -0400

    Added nested template example

---
+++ 10_nested_templates/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+// Syntax reads package.Type||Function
+var temp *template.Template
+
+func init() {
+	// Must gaves back the pointer template
+	temp = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+
+	err := temp.ExecuteTemplate(os.Stdout, "temp.gohtml", 42)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
